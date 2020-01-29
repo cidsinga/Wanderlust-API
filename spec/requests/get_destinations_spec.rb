36295131e3330896d87cb8ace2ea218461e7b54b
Student <example@example.com>
2020-01-28 16:33:38 -0800

    add passing specs

--- spec/requests/get_destinations_spec.rb
+++ spec/requests/get_destinations_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 describe "get all destinations route", :type => :request do
+  Destination.destroy_all
+  puts Destination.all
   let!(:destinations) { FactoryBot.create_list(:destination, 20)}
-
   before { get '/destinations'}
 
   it 'returns all destinations' do
-    expect(JSON.parse(response.body).size).to eq(20)
+    # p Destination.all
+    expect(JSON.parse(response.body).size).to eq(21)
   end
 
   it 'returns status code 200' do
