8ff56bd991b9824cd4a9ac032da079826f2d19d1
Student <example@example.com>
2020-01-30 10:44:23 -0800

    full review crud

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,7 +11,27 @@ class ReviewsController < ApplicationController
     json_response(@review)
   end
 
+  def create
+    @review = Review.create!(review_params)
+    json_response(@review, :created)
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update!(review_params)
+      render status: 200, json: {
+        message: "This review has been updated successfully."
+      }
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+  end
+
   private
-
-
+  def review_params
+    params.permit(:content, :author, :rating, :destination_id)
+  end
 end
